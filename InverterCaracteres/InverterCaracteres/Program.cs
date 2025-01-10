358f058ab96cb0d6b8ea05dff27bb282fbfed380
Gabriel Lopes <96749828+gabriellops@users.noreply.github.com>
2025-01-10 12:24:13 -0300

    Add inverter caracteres

---
+++ InverterCaracteres/InverterCaracteres/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Digite uma palavra para ser invertida:");
+var input = Console.ReadLine().Trim();
+
+Inverter(input);
+Console.ReadLine();
+
+static void Inverter(string input)
+{
+    string str = "";
+
+    for (int i = input.Length - 1; i >= 0; i--)
+        str += input[i];
+
+    Console.WriteLine($"Palavra invertida: {str}");
+}
